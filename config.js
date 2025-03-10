acbb3bddb76cfc66f4c0955ab9bbba497daef904
Tovi <T0556729726@gmail.com>
2025-03-11 00:35:20 +0200

    finish

--- config.js
+++ config.js
@@ -4,8 +4,6 @@ dotenv.config();
 
 const config = {
     port: process.env.PORT || 5000,
-    // mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/defaultdb",
-    // jwtSecret: process.env.JWT_SECRET || "defaultSecretKey",
   };
 
 export default config;
