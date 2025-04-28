1c3254c9d973072b1ec165b6eec74b8b5453bfcb
Tovi <T0556729726@gmail.com>
2025-04-28 11:11:33 +0300

    again and again

--- src/device.js
+++ src/device.js
@@ -1,17 +1,17 @@
+// *************** Require External Modules ****************//
 import e from "express";
-import { getStatusByDeviceId, getDeviceInfo, toggleDevice, updateDeviceName } from "./lib/storage/tuya.js";
-import AppError from "./lib/appError.js";
-import { updateDeviceStatus, printDeviceStatus } from "./lib/storage/mongo.js";
-import { createResponse } from "./lib/response.js";
-
-// **** Import necessary dependencies ****
 import bcrypt from "bcrypt";
-import generateToken from "./lib/utils/generateToken.js";
 import Ajv from "ajv";
 import addFormats from "ajv-formats";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+
+// **** Import necessary dependencies ****
+import { getStatusByDeviceId, toggleDevice } from "./lib/storage/tuya.js";
+import { findDevicesAndFamilyNameByfamily_id } from "./lib/storage/mongo.js";
+import AppError from "./lib/appError.js";
+import { createResponse } from "./lib/response.js";
 
 // **** Load JSON Schemas using Ajv ****
 const ajv = new Ajv({ strict: false, allErrors: true });
@@ -31,28 +31,25 @@ fs.readdirSync(schemasDir).forEach((file) => {
 const validateSchema = (schemaId, data) => {
   const validate = ajv.getSchema(schemaId);
   if (!validate) {
-    throw new Error(`Schema ${schemaId} not found`);
+    throw new AppError(`Schema ${schemaId} not found`, 404);
   }
   const isValid = validate(data);
   return { isValid, errors: validate.errors };
 };
-
-// Import MongoDB helper functions from mongo.js
-import { findDevicesAndFamilyNameByfamily_id } from "./lib/storage/mongo.js";
 
 // Get all devices belonging to the family and the family name
 async function getDeviceListAndFamilyNameByfamily_id(req, res, next) {
   try {
     const { family_id } = req.query;
     if (!family_id) {
-      return res.status(401).json({ message: "family_id is required" });
+      throw new AppError("Family ID is required.", 400);
     }
     const response = await findDevicesAndFamilyNameByfamily_id(family_id);
     console.log("response: ", response);
     if (response == null) {
       return res.status(404).json({ message: "Family not found" });
     }
-    return res.status(200).json({ message: "Devices and family name fetched successfully", response });
+    return res.status(200).json(createResponse(true,"Devices and family name fetched successfully", response)) ;
   } catch (error) {
     next(error);
   }
