2fb1f9e3158e694cb4e5a17f0799065078e09929
tovi-kos <T0556729726@gmail.com>
2025-06-05 09:45:39 +0300

    old changes

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const config = {
-    port: process.env.PORT || 5000,
-  };
+// const config = {
+//     port: process.env.PORT || 5000,
+//   };
 
-export default config;
+// export default config;
